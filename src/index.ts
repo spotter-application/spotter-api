469cd10c4013b83abab62ce4a5ac5a79dfd62712
Denis Z <zzulev@gmail.com>
2021-09-01 15:59:37 +0200

    Add output method

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,39 @@ declare var process : {
   argv: Array<string>;
 }
 
+interface Option {
+  id: string;
+  title: string;
+  action: string;
+}
+
 const inputCommand = process.argv[2];
 const inputQuery = process.argv.splice(3).join(' ');
 
 export const Spotter = {
-  onQuery: (query: string) => {
+  output: (options: Option[]) => {
+    if (inputCommand !== 'query' || !inputQuery) {
+      return;
+    }
 
+    console.log(
+      JSON.stringify(options.filter(option => option.title.toLowerCase().startsWith(inputQuery.toLowerCase())))
+    );
+  },
+
+  onQuery: (query: string) => {
     return {
-      output: (callback: (query: string) => void) => {
+      output: (callback: (query: string) => Option[]) => {
         if (inputCommand !== 'query' || !inputQuery || !query.toLowerCase().startsWith(inputQuery.toLowerCase())) {
           return;
         }
 
         // TODO: validate
-        console.log(callback(query));
+        console.log(JSON.stringify(callback(query)));
       }
     }
   },
+
   onAction: (actionName: string) => {
     return {
       run: (callback: () => void) => {
